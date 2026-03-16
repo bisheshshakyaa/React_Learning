7bf4efc676fa17ef24acee14117cecddefdd70f1
Bishesh <bisheshshakya123@gmail.com>
2026-03-16 15:57:38 +0545

    Create pages using react-router

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { HomePage } from "./pages/HomePage";
 import "./App.css";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="checkout" element={<div>Test Checkout Page</div>}/>
+    </Routes>
   );
 }
 
